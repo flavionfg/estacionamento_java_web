013dadccb969a937c47cdc81768970cdb866e8ea
Senai <Senai@SNLDN09LABF03>
2019-03-14 14:44:39 -0300

    Crud Professor

---
+++ Estacionamento/src/br/com/estacionamento/mvc/persistent_object/AbstractCRUD.java
@@ -0,0 +1,32 @@
+package br.com.estacionamento.mvc.persistent_object;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class AbstractCRUD {
+	
+	EntityManagerFactory emf = null;
+	EntityManager em = null;
+	
+	
+	public void open() {
+		
+		this.emf = Persistence.createEntityManagerFactory("ESTACIONAMENTO_PU");
+		this.em = this.emf.createEntityManager();
+	}
+	
+	public void close() {
+		this.em.close();
+		this.emf.close();
+		
+	}
+
+	public abstract void insert(Object o);
+	public abstract void update(Object o);
+	public abstract void delete(Object o);
+	public abstract ArrayList<Object> select(String statament);
+	
+}
